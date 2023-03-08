6d123ca2ce577826316b8b11b18c96f85fa3a18a
Christian Gravel <cjgravel@gmail.com>
2023-03-07 22:29:04 -0800

    completed Assert Arrays Equal function

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(array1, array2) {
+  let result = true;
+  if (array1.length === array2.length) {
+    for(let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} == ${array2}`);
+    } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} != ${array2}`);
+  } 
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 5, 3]);
+
