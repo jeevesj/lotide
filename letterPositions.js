e82b32980a8fcfdf71e1564544fc3db417d98a16
Christian Gravel <cjgravel@gmail.com>
2023-03-08 22:02:13 -0800

    record the letter positions of a string

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} == ${array2}`);
+    } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} != ${array2}`);
+  } 
+};
+
+const eqArrays = function(array1, array2) {
+  let result = true;
+  if (array1.length === array2.length) {
+    for(let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    if (char !== " ") {
+      if (!results[char]) {
+        results[char] = [];
+      }
+      results[char].push(i);
+    }
+
+  } 
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
