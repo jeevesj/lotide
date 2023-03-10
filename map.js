4b03c8e2acf8794483f27a777267f7f906110087
Christian Gravel <cjgravel@gmail.com>
2023-03-09 19:56:05 -0800

    create a map function

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  let result = true;
+  if (array1.length === array2.length) {
+    for(let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} == ${array2}`);
+    } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} != ${array2}`);
+  } 
+};
+
+
+//const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+//const results1 = map(words, word => word[0]);
+//console.log(results1);
