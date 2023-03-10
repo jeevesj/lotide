1407263640a9eea83bc9a654c49eeeb6bd784e23
Christian Gravel <cjgravel@gmail.com>
2023-03-09 22:15:54 -0800

    find first value with truthy characteristics and return the corressponding key

---
+++ findKey.js
@@ -0,0 +1,28 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+function findKey(data, callback) {
+  newKeys = Object.keys(data);
+  newValues = Object.values(data);
+  for (let i = 0; i < newKeys.length; i++) {
+    if(callback(newValues[i])) {
+      return newKeys[i];
+    }
+  }
+};
+
+
+// console.log(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2)); // => "noma"
